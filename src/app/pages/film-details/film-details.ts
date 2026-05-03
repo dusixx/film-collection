3e8b24883f7ccd1910c710997c407689c0b6522a
dusixx <113414935+dusixx@users.noreply.github.com>
2026-05-03 09:01:54 +0300

    feat: implement FilmDetails page

--- src/app/pages/film-details/film-details.ts
+++ src/app/pages/film-details/film-details.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FilmService } from '@app/services/film.service';
+import { DurationPipe, FormatThousandsPipe, rndInt } from '@app/shared';
 
 @Component({
-  selector: 'app-film-details',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, DurationPipe, FormatThousandsPipe],
   templateUrl: './film-details.html',
   styleUrl: './film-details.scss',
 })
-export class FilmDetails {}
+export class FilmDetails {
+  private route = inject(ActivatedRoute);
+  private service = inject(FilmService);
+
+  rawId = this.route.snapshot.paramMap.get('id');
+  id = Number(this.rawId);
+  film = computed(() => this.service.getFilmById(this.id));
+  votes = rndInt(50_000, 500_000);
+
+  handleToggleFavorite() {
+    this.service.toggleFavorite(this.film()!.id);
+  }
+}
