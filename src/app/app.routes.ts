bb30221e7699d18525e00dbeccfd193d2c8d6bd5
dusixx <113414935+dusixx@users.noreply.github.com>
2026-05-04 06:54:45 +0300

    feat: update app routes structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,36 @@
 import { Routes } from '@angular/router';
-import { About, FilmDetails, Home, NotFound } from '@pages';
+import { filmTitleResolver } from './core/resolvers/film-resolver';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'films', pathMatch: 'full' },
-  { path: 'films', component: Home },
-  { path: 'about', component: About },
-  { path: 'films/:id', component: FilmDetails },
-  { path: '**', component: NotFound },
+  {
+    path: '',
+    redirectTo: 'films',
+    pathMatch: 'full',
+  },
+  {
+    path: 'films',
+    data: { label: 'Home' },
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('@features/home/home').then((m) => m.Home),
+        data: { label: 'Home' },
+      },
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('@features/film-details/film-details').then((m) => m.FilmDetails),
+        resolve: { label: filmTitleResolver },
+      },
+    ],
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('@features/about/about').then((m) => m.About),
+    data: { label: 'About' },
+  },
+  {
+    path: '**',
+    loadComponent: () => import('@features/not-found/not-found').then((m) => m.NotFound),
+  },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer } from './components/footer/footer.js';
-import { Header } from './components/header/header.js';
+import { Footer } from './shared/components/footer/footer.js';
+import { Header } from './shared/components/header/header.js';
+import { Breadcrumbs } from './shared/components/breadcrumbs/breadcrumbs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Breadcrumbs],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
