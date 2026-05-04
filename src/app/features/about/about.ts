5903744d968fb2b9111bdedefbc15e3b2961dcb3
dusixx <113414935+dusixx@users.noreply.github.com>
2026-05-04 11:51:41 +0300

    feat: add back button to about

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { HOME_BTN_TEXT } from '@app/shared';
@@ -11,5 +12,11 @@ import { Button } from '@app/shared/components/button/button';
 })
 export class About {
   router = inject(Router);
+  location = inject(Location);
+
   homeBtnText = HOME_BTN_TEXT;
+
+  goBack() {
+    this.location.back();
+  }
 }
