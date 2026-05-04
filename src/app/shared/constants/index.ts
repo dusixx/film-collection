6a08db5705a061ae2b0a50a4ed3423e079a2f287
dusixx <113414935+dusixx@users.noreply.github.com>
2026-05-04 07:29:36 +0300

    feat: add constants

---
+++ src/app/shared/constants/index.ts
@@ -0,0 +1,15 @@
+export const RouteData = {
+  Home: {
+    path: 'films',
+    label: 'Home',
+  },
+  About: {
+    path: 'about',
+    label: 'About',
+  },
+} as const;
+
+export const AuthorInfo = {
+  GithubUsername: 'dusixx',
+  GithubRepoUrl: 'https://github.com/dusixx/film-collection',
+};
