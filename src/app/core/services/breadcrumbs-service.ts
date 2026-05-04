5b41b82354317396868ea8dd85d76ab73a73c67f
dusixx <113414935+dusixx@users.noreply.github.com>
2026-05-04 09:05:30 +0300

    fix: remove unused imports

--- src/app/core/services/breadcrumbs-service.ts
+++ src/app/core/services/breadcrumbs-service.ts
@@ -1,4 +1,4 @@
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Breadcrumb } from '../models/breadcrumb.model';
 
