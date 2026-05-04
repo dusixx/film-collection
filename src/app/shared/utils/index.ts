22ee93e78a5b8f96f40cda97d46375f30eb8f021
dusixx <113414935+dusixx@users.noreply.github.com>
2026-05-04 13:09:35 +0300

    chore: clean up

--- src/app/shared/utils/index.ts
+++ src/app/shared/utils/index.ts
@@ -1,2 +1,40 @@
-export * from './misc.utils';
-export * from './type-guards.utils';
+import { ERR_UNKNOWN_ERROR, NOT_AVAILABLE } from '../constants';
+
+export const isString = (obj: unknown): obj is string => {
+  return typeof obj === 'string';
+};
+
+export const isError = (obj: unknown): obj is Error => {
+  return obj instanceof Error;
+};
+
+export const getErrorMessage = (e: unknown): string => {
+  return isError(e) ? e.message : isString(e) ? e : ERR_UNKNOWN_ERROR;
+};
+
+export const minutesToDuration = (mins: number): string => {
+  if (mins <= 0) {
+    return NOT_AVAILABLE;
+  }
+  const hours = Math.floor(mins / 60);
+  const minutes = mins % 60;
+
+  if (!minutes) {
+    return `${hours}h`;
+  }
+  if (!hours) {
+    return `${minutes}min`;
+  }
+  return `${hours}h ${minutes}min`;
+};
+
+export const rndInt = (min: number, max: number): number => {
+  return Math.round(min + Math.random() * (max - min));
+};
+
+export const formatThousands = (num: number, separator = ' '): string => {
+  const formatter = new Intl.NumberFormat('fr-FR', {
+    maximumFractionDigits: 0,
+  });
+  return formatter.format(num).replace(/[^\d]/g, separator);
+};
